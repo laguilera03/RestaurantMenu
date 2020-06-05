de25f36a96adbb81ca45a5a9de6dd09a549d6b5e
lawrence <laguilera03@gmail.com>
2020-06-05 14:33:29 -0400

    Initialized the Project and Classes

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        /*
+         * Menu should include current date, display items, and ability to input new items and pass it to the menu items class list/dictionary
+         */
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        /*
+         * MenuItems should be the class holding all the items price, description, and category, including the date (to match with date last modified), privately, and the date it was last modified
+         */
+    }
+}
